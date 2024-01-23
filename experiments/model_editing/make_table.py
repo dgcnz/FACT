73cf8e11f16338a6597e4404d1cba8d5a0e14447
Diego Canez <canezdiego@gmail.com>
2024-01-23 12:03:25 +0100

    refactor: make_table cleanup

--- experiments/model_editing/make_table.py
+++ experiments/model_editing/make_table.py
@@ -10,25 +10,6 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def setup_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--base_config", type=str, default=CONFIG_PATH / "base_clip_resnet50.yaml"
-    )
-    parser.add_argument("--device", type=str, default="mps")
-    parser.add_argument("--seed", type=int, default=42)
-    return parser
-
-
-def get_all_configs():
-    """Get all folders in CONFIG_PATH"""
-    configs = []
-    for config in CONFIG_PATH.iterdir():
-        if config.is_dir():
-            configs.append(config)
-    return sorted(configs)
-
-
 class LightningScript(object):
     entrypoint: list[str]
 
@@ -125,7 +106,7 @@ class LightningScript(object):
         return test_accuracy
 
 
-def train_and_test_2(base_config: Path, config_folder: Path, device: str, seed: int):
+def train_and_test(base_config: Path, config_folder: Path, device: str, seed: int):
     script = LightningScript()
     base_config_path = config_folder / "base.yaml"
     prune_config_path = config_folder / "prune.yaml"
@@ -168,45 +149,24 @@ def train_and_test_2(base_config: Path, config_folder: Path, device: str, seed: 
     logger.info(f"Accuracy improvement: {pruned_test_accuracy - base_test_accuracy}")
     return base_test_accuracy, pruned_test_accuracy
 
-
-def train_and_test(base_config: Path, config_folder: Path, device: str, seed: int):
-    base_config_path = config_folder / "base.yaml"
-    prune_config_path = config_folder / "prune.yaml"
-    task_name = config_folder.stem
-
-    entrypoint = ["python", "-m", "experiments.model_editing.main"]
-    basic_args = ["--trainer.accelerator", device, "-c", base_config]
-    command = entrypoint + ["fit"] + basic_args + ["-c", base_config_path]
-    print("Training")
-    run = subprocess.run(command, capture_output=True, text=True)
-    modelpath_pattern = re.compile(r"MODELPATH=(.*)")
-    test_accuracy_pattern = re.compile(r"test_accuracy.*(\d+\.\d+)")
-
-    path = Path(modelpath_pattern.search(run.stdout).group(1)).readlink()
-    print("Running baseline")
-    command = (
-        entrypoint
-        + ["test"]
-        + basic_args
-        + ["-c", base_config_path]
-        + ["--ckpt_path", path]
+def setup_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--base_config", type=str, default=CONFIG_PATH / "base_clip_resnet50.yaml"
     )
-    run = subprocess.run(command, capture_output=True, text=True)
-    test_accuracy_base = test_accuracy_pattern.search(run.stdout).group(1)
-    test_accuracy_base = float(test_accuracy_base)
-    print("Running prune")
+    parser.add_argument("--device", type=str, default="mps")
+    parser.add_argument("--seed", type=int, default=42)
+    return parser
 
-    command = (
-        entrypoint
-        + ["test"]
-        + basic_args
-        + ["-c", base_config_path, "-c", prune_config_path]
-        + ["--ckpt_path", path]
-    )
-    run = subprocess.run(command, capture_output=True, text=True)
-    test_accuracy_prune = test_accuracy_pattern.search(run.stdout).group(1)
-    test_accuracy_prune = float(test_accuracy_prune)
-    print(f"ACC IMPROVEMENT {test_accuracy_base} -> {test_accuracy_prune}")
+
+def get_all_configs():
+    """Get all folders in CONFIG_PATH"""
+    configs = []
+    for config in CONFIG_PATH.iterdir():
+        if config.is_dir():
+            configs.append(config)
+    return sorted(configs)
+
 
 
 def main():
@@ -214,7 +174,7 @@ def main():
     args = parser.parse_args()
     configs = get_all_configs()
     for config_folder in configs:
-        train_and_test_2(
+        train_and_test(
             base_config=args.base_config,
             config_folder=config_folder,
             device=args.device,
