ea7689b3cb19b05994e19a0641c815e7944d054d
Diego Canez <canezdiego@gmail.com>
2024-01-23 14:18:13 +0100

    feat: add custom pcbm loss

--- models/pcbm_pl.py
+++ models/pcbm_pl.py
@@ -1,5 +1,7 @@
 import torch
 import lightning as L
+from typing import Literal
+import logging
 
 
 class PCBMClassifier(torch.nn.Module):
@@ -17,23 +19,89 @@ class PCBMClassifier(torch.nn.Module):
             self.classifier.weight[class_ix, concept_ix] = 0.0
 
 
-class PCBMClassifierTrainer(PCBMClassifier, L.LightningModule):
+class PCBMLoss(torch.nn.Module):
+    def __init__(
+        self,
+        lam: float,
+        n_concepts: int,
+        n_classes: int,
+        alpha: float,
+        classifier: torch.nn.Module,
+        reduction: Literal["sum", "mean"] = "sum",
+    ):
+        """
+        Calculates the PCBM loss with elastic net regularization
+        weighted by lam / (n_classes * n_concepts)
+
+        Args:
+        :param lam: numerator in elastic net constant
+        :param n_concepts: number of concepts used
+        :param n_classes: number of classes used
+        :param alpha: l1 to l2 ratio in elastic net
+        :param classifier: classifier to optimize
+        :param reduction: reduction to use for loss function
+        """
+        super(PCBMLoss, self).__init__()
+        self.lam = lam
+        self.n_concepts = n_concepts
+        self.n_classes = n_classes
+        self.alpha = alpha
+        self.classifier = classifier
+        self.reduction = reduction
+        self.reduce_fn = torch.sum if self.reduction == "sum" else torch.mean
+        self.elastic_net_constant = self.lam / (self.n_classes * self.n_concepts)
+
+    def _per_sample_elastic_net(self, param: torch.Tensor):
+        """
+        Calculates the elastic net for a single parameter
+        """
+        l1_norm = torch.norm(param, p=1)
+        l2_norm = torch.norm(param, p=2)
+        return self.alpha * l1_norm + (1 - self.alpha) * l2_norm
+
+    def forward(self, y_hat: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+        """
+        Calculates the PCBM loss
+        """
+        base_loss = torch.nn.functional.cross_entropy(
+            y_hat, y, reduction=self.reduction
+        )
+        elastic_net = self.reduce_fn(
+            torch.stack(
+                [
+                    self._per_sample_elastic_net(param)
+                    for param in self.classifier.parameters()
+                ]
+            )
+        )
+        return base_loss + self.elastic_net_constant * elastic_net
+
+
+class PCBMClassifierTrainer(L.LightningModule):
     def __init__(
         self,
         n_concepts: int,
         n_classes: int,
         lr: float,
-        weight_decay: float,
+        lam: float,
+        alpha: float,
         pruned_concept_class_pairs: list[tuple[int, int]] = [],
         normalize: bool = False,
     ):
-        super(PCBMClassifierTrainer, self).__init__(
-            n_concepts=n_concepts, n_classes=n_classes
-        )
+        super().__init__()
+        self.model = PCBMClassifier(n_concepts=n_concepts, n_classes=n_classes)
         self.lr = lr
-        self.weight_decay = weight_decay
         self.pruned_concept_class_pairs = pruned_concept_class_pairs
         self.normalize = normalize
+        self.lam = lam
+        self.alpha = alpha
+        self.loss = PCBMLoss(
+            lam=self.lam,
+            n_concepts=n_concepts,
+            n_classes=n_classes,
+            alpha=self.alpha,
+            classifier=self.model.classifier,
+        )
         assert (
             not self.normalize or self.pruned_concept_class_pairs
         ), "If normalize is true, pruned_concept_class_pairs must be nonempty"
@@ -41,8 +109,8 @@ class PCBMClassifierTrainer(PCBMClassifier, L.LightningModule):
 
     def training_step(self, batch, batch_idx):
         x, y = batch["concept_weights"], batch["label"]
-        y_hat = self(x)
-        loss = torch.nn.functional.cross_entropy(y_hat, y)
+        y_hat = self.model(x)
+        loss = self.loss(y_hat, y)
         accuracy = torch.sum(torch.argmax(y_hat, dim=1) == y) / len(y)
         self.log("train_accuracy", accuracy)
         self.log("train_loss", loss)
@@ -50,8 +118,8 @@ class PCBMClassifierTrainer(PCBMClassifier, L.LightningModule):
 
     def validation_step(self, batch, batch_idx):
         x, y = batch["concept_weights"], batch["label"]
-        y_hat = self(x)
-        loss = torch.nn.functional.cross_entropy(y_hat, y)
+        y_hat = self.model(x)
+        loss = self.loss(y_hat, y)
         accuracy = torch.sum(torch.argmax(y_hat, dim=1) == y) / len(y)
         self.log("val_loss", loss)
         self.log("val_accuracy", accuracy)
@@ -59,22 +127,24 @@ class PCBMClassifierTrainer(PCBMClassifier, L.LightningModule):
 
     def test_step(self, batch, batch_idx):
         x, y = batch["concept_weights"], batch["label"]
-        y_hat = self(x)
+        y_hat = self.model(x)
         accuracy = torch.sum(torch.argmax(y_hat, dim=1) == y) / len(y)
         self.log("test_accuracy", accuracy)
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(
-            self.parameters(), lr=self.lr, weight_decay=self.weight_decay
-        )
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
         return optimizer
 
     def load_state_dict(self, *args, **kwargs):
+        logging.debug(f"Calling custom load_state_dict with {self.pruned_concept_class_pairs}")
         super().load_state_dict(*args, **kwargs)
         for concept_ix, class_ix in self.pruned_concept_class_pairs:
-            previous_weight = self.classifier.weight[class_ix, concept_ix].item()
-            self.prune(concept_ix=concept_ix, class_ix=class_ix)
-            zero_weight = self.classifier.weight[class_ix, concept_ix].item()
+            previous_weight = self.model.classifier.weight[class_ix, concept_ix].item()
+            self.model.prune(concept_ix=concept_ix, class_ix=class_ix)
+            zero_weight = self.model.classifier.weight[class_ix, concept_ix].item()
+            logging.debug(
+                f"Pruned {concept_ix} {class_ix} from {previous_weight} to {zero_weight}"
+            )
             assert zero_weight == 0.0, zero_weight
             assert previous_weight != 0.0, previous_weight
         if self.normalize:
