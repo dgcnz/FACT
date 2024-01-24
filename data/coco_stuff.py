fd0465987af06ce5cbf1b9922d9201dfa79d1dbb
Gregory Go <gregorygocc@gmail.com>
2024-01-24 19:55:55 +0100

    temp: COCO-Stuff trimmed dataset tests

--- data/coco_stuff.py
+++ data/coco_stuff.py
@@ -4,7 +4,7 @@ import numpy as np
 from PIL import Image
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
-from constants import COCO_STUFF_DIR
+from .constants import COCO_STUFF_DIR
 
 
 # By default focuses on the 20 most biased classes mentioned in Singh et al., 2020
@@ -25,7 +25,7 @@ class COCODataset(Dataset):
 
     def __getitem__(self, idx):
         img_data = self.data[idx]
-        img_path = img_data[0]
+        img_path = os.path.join(COCO_STUFF_DIR, img_data[0])
         img = Image.open(img_path).convert('RGB')
         img = transforms.ToTensor()(img)
         class_label = img_data[1]
@@ -34,6 +34,7 @@ class COCODataset(Dataset):
 
         return img, class_label
 
+# function for converting 'coco_target_indexes.txt" to a dict
 def cid_to_class(pathtxt, target_labs): # "cid = COCO ID"
     labeldict = {}
     with open(pathtxt) as f:
@@ -44,29 +45,11 @@ def cid_to_class(pathtxt, target_labs): # "cid = COCO ID"
 
     return labeldict
 
-def idx2img(json_file, image_dir):
-    """
-    Arguments:
-    json_file: the annotation JSON file loaded by load_coco_data below
-    image_dir: the image dir passed to load_coco_data
-    
-    Outputs:
-    img_dict: dictionary containing mappings of '{<image index>: image_dir + <image filename>}'
-    """
-    img_dict = {}
-    for data in json_file['images']:
 
-        id = data['id']
-        name = data['file_name']
-        img_dict[id] = os.path.join(image_dir, name)
-
-    return img_dict
-
-def load_coco_data(image_dir, annot_dir, target:int, n_samples:int=500,
+def load_coco_data(annot_dir, target:int, n_samples:int=500,
                    transform=None, batch_size:int=1, seed:int=42):
     """
     Arguments:
-    image_dir: directory containing the images to load (either train2017/val2017)
     annot_dir: directory of the corresponding annotation file
     transform: whether to apply any special transformation. Default = None
     target: the class index of the target class (see below for guide)
@@ -83,29 +66,14 @@ def load_coco_data(image_dir, annot_dir, target:int, n_samples:int=500,
     f = open(annot_dir)
     data = json.load(f)
 
-    # get the annotation index to filename mapping
-    img_dict = idx2img(data, image_dir)
-
     # even split between training and test data
     npc = n_samples // 2
-    img_idx_dict = {}
-
-    # extract the data
-    for segment in data['annotations']:
-        class_id = segment['category_id']
-        image_id = img_dict[segment['image_id']]
-        if image_id not in img_idx_dict.keys():
-            img_idx_dict[image_id] = [class_id]
-        elif class_id not in img_idx_dict[image_id]:
-            img_idx_dict[image_id] += [class_id]
-        else:
-            continue
         
     # select images based on whether not the image has a segmentation of the target object
     pos_data = []
     neg_data = []
-    for imgc in img_idx_dict.keys():
-        if target in img_idx_dict[imgc]:
+    for imgc in data.keys():
+        if target in data[imgc]:
             pos_data.append(imgc)
         else:
             neg_data.append(imgc)
@@ -127,13 +95,11 @@ def load_coco_data(image_dir, annot_dir, target:int, n_samples:int=500,
 
 # For testing
 if __name__ == "__main__":
-    ltr_path = os.path.join(COCO_STUFF_DIR, "annotations/instances_train2017.json")
-    lt_path = os.path.join(COCO_STUFF_DIR, "annotations/instances_val2017.json")
-    train_path = os.path.join(COCO_STUFF_DIR, "train2017")
-    test_path = os.path.join(COCO_STUFF_DIR, "val2017")
+    ltr_path = os.path.join(COCO_STUFF_DIR, "labels_train.json")
+    lt_path = os.path.join(COCO_STUFF_DIR, "labels_val.json")
 
-    train_coco = load_coco_data(train_path, ltr_path, target=41, n_samples=500)
-    test_coco = load_coco_data(test_path, lt_path, target=41, n_samples=250)
+    train_coco = load_coco_data(ltr_path, target=41, n_samples=500)
+    test_coco = load_coco_data(lt_path, target=41, n_samples=250)
 
     first_x, first_y = next(iter(test_coco))
     
