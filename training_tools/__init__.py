f321e92ab7be18a9b327be8eac6545cc187f3550
Nesta <n.midavaine@student.vu.nl>
2024-01-29 12:55:01 +0100

    fix:

--- training_tools/__init__.py
+++ training_tools/__init__.py
@@ -1,109 +1,66 @@
-import os
-import json
 import numpy as np
-from PIL import Image
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms
-from .constants import COCO_STUFF_DIR
+from sklearn.metrics import confusion_matrix
+from .utils import *
+from .embedding_tools import load_or_compute_projections
 
 
-# By default focuses on the 20 most biased classes mentioned in Singh et al., 2020
-class COCODataset(Dataset):
+class AverageMeter(object):
+    def __init__(self):
+        self.reset()
 
-    def __init__(self, datalist, transform=None):
-        """
-        Arguments:
-        datalist: a list object instance returned by 'load_coco_data' function below
-        transform: whether to apply any special transformation. Default = None
-        """
-        self.data = datalist
-        self.transform = transform
-        self.num_classes = 2 # By default these datasets are always for binary classification
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
 
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, idx):
-        img_data = self.data[idx]
-        img_path = os.path.join(COCO_STUFF_DIR, img_data[0])
-        img = Image.open(img_path).convert('RGB')
-        class_label = img_data[1]
-        if self.transform:
-            img = self.transform(img)
-
-        return img, class_label
-
-# function for converting 'coco_target_indexes.txt" to a dict for binary classification
-def cid_to_class(pathtxt, target_labs, target_idx): # "cid = COCO ID"
-    labeldict = {}
-    with open(pathtxt) as f:
-        for line in f.readlines():
-            label, idx = line.strip("\n").split(": ")
-            if (label in target_labs) or (target_labs is None):
-                if int(idx) == target_idx:
-                   labeldict[1] = label
-                else:
-                   labeldict[0] = 'other'
-
-    return labeldict
-
-
-def load_coco_data(annot_dir, target:int, n_samples:int=500,
-                   transform=None, batch_size:int=1, seed:int=42):
-    """
-    Arguments:
-    annot_dir: directory of the corresponding annotation file
-    transform: whether to apply any special transformation. Default = None
-    target: the class index of the target class (see below for guide)
-    n_samples: number of samples to take (default 250, should be 500 for training)
-    seed: random seed for sampling
-
-    Outputs:
-    dataloader: torch dataloader instance containing the data
-    """
-    # set the seeds
-    np.random.seed(seed)
-
-    # open the file
-    f = open(annot_dir)
-    data = json.load(f)
-
-    # even split between training and test data
-    npc = n_samples // 2
-        
-    # select images based on whether not the image has a segmentation of the target object
-    pos_data = []
-    neg_data = []
-    for imgc in data.keys():
-        if target in data[imgc]:
-            pos_data.append(imgc)
-        else:
-            neg_data.append(imgc)
-
-    # sample with replacement if too little data is present    
-    replace = True if (len(pos_data) < npc) else False
-    pos_sample = np.random.choice(pos_data, size=npc, replace=replace)
-    neg_sample = np.random.choice(pos_data, size=npc, replace=replace)
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
     
-    pos_sample = [[pos_sample[idx], 1] for idx in range(npc)]
-    neg_sample = [[neg_sample[idx], 0] for idx in range(npc)]
-
-    datalist = pos_sample + neg_sample
-    dataset = COCODataset(datalist, transform)
-    dataloader = DataLoader(dataset, batch_size=batch_size)
-
-    return dataloader
+    def __str__(self):
+        return f'AverageMeter(val={self.val}, avg={self.avg}, sum={self.sum}, count={self.count})'
 
 
-# For testing
-if __name__ == "__main__":
-    ltr_path = os.path.join(COCO_STUFF_DIR, "labels_train.json")
-    lt_path = os.path.join(COCO_STUFF_DIR, "labels_val.json")
-
-    train_coco = load_coco_data(ltr_path, target=41, n_samples=500)
-    test_coco = load_coco_data(lt_path, target=41, n_samples=250)
-
-    first_x, first_y = next(iter(test_coco))
+class MetricComputer(object):
+    def __init__(self, metric_names=None, n_classes=5):
+        __all_metrics__ = {"accuracy": self._accuracy, 
+                            "class-level-accuracy": self._class_level_accuracy,
+                            "confusion_matrix": self._confusion_matrix}
+        all_names = list(__all_metrics__.keys())
+        if metric_names is None:
+            metric_names = all_names
+        for n in metric_names: assert n in all_names
+        self.metrics = {m: __all_metrics__[m] for m in metric_names}
+        self.n_classes = n_classes
     
-    print(first_x)
-    print(first_y)
+    def __call__(self, out, target):
+        """
+        Args:
+            out (torch.Tensor): Model output
+            target (torch.Tensor): Target labels
+        """
+        pred = out.argmax(dim=1)
+        result = {m: self.metrics[m](out, pred, target) for m in self.metrics.keys()}
+        return result
+    
+    def _accuracy(self, out, pred, target):
+        acc = (pred == target).float().detach().mean()
+        return acc.item()
+
+    def _class_level_accuracy(self, out, pred, target):
+        per_class_acc = {}
+        for c in range(self.n_classes):
+            count = (target == c).sum().detach().item()
+            if count == 0:
+                continue
+            class_true = ((pred == target) * (target == c)).float().sum().item()
+            per_class_acc[c] = (class_true, count)
+        return per_class_acc
+    
+    def _confusion_matrix(self, out, pred, target):
+        y_true = target.detach().cpu()
+        y_pred = pred.detach().cpu()
+        return confusion_matrix(y_true, y_pred, normalize=None, labels=np.arange(self.n_classes))
